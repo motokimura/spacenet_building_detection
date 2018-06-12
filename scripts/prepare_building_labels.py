467ed3dd2e2a359885eefba365fca05844ad30e2
motokimura <motokimura1@gmail.com>
2018-06-12 23:49:03 +0900

    Replace indentations with tabs

--- scripts/prepare_building_labels.py
+++ scripts/prepare_building_labels.py
@@ -13,33 +13,33 @@ from create_poly_mask import create_poly_mask
 
 def prepare_building_labels(src_raster_dir, src_vector_dir, dst_dir):
 
-    file_count = len([f for f in os.walk(src_vector_dir).__next__()[2] if f[-8:] == ".geojson"])
+	file_count = len([f for f in os.walk(src_vector_dir).__next__()[2] if f[-8:] == ".geojson"])
 
-    print("[INFO] Found {} geojson files. Preparing building mask images...".format(file_count))
+	print("[INFO] Found {} geojson files. Preparing building mask images...".format(file_count))
 
-    for idx in tqdm(range(1, file_count + 1)):
+	for idx in tqdm(range(1, file_count + 1)):
 
-        src_raster_filename = "3band_AOI_1_RIO_img{}.tif".format(idx)
-        src_vector_filename = "Geo_AOI_1_RIO_img{}.geojson".format(idx)
+		src_raster_filename = "3band_AOI_1_RIO_img{}.tif".format(idx)
+		src_vector_filename = "Geo_AOI_1_RIO_img{}.geojson".format(idx)
 
-        src_raster_path = os.path.join(src_raster_dir, src_raster_filename)
-        src_vector_path = os.path.join(src_vector_dir, src_vector_filename)
-        dst_path = os.path.join(dst_dir, src_raster_filename)
+		src_raster_path = os.path.join(src_raster_dir, src_raster_filename)
+		src_vector_path = os.path.join(src_vector_dir, src_vector_filename)
+		dst_path = os.path.join(dst_dir, src_raster_filename)
 
-        create_poly_mask(
-            src_raster_path, src_vector_path, npDistFileName=dst_path, 
-            noDataValue=0, burn_values=255
-        )
+		create_poly_mask(
+			src_raster_path, src_vector_path, npDistFileName=dst_path, 
+			noDataValue=0, burn_values=255
+		)
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser()
 
-    parser.add_argument('src_raster_dir', help='Root directory for raster files (.tif)')
-    parser.add_argument('src_vector_dir', help='Root directory for vector files (.geojson)')
-    parser.add_argument('dst_dir', help='Output directory')
+	parser.add_argument('src_raster_dir', help='Root directory for raster files (.tif)')
+	parser.add_argument('src_vector_dir', help='Root directory for vector files (.geojson)')
+	parser.add_argument('dst_dir', help='Output directory')
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    prepare_building_labels(args.src_raster_dir, args.src_vector_dir, args.dst_dir)
+	prepare_building_labels(args.src_raster_dir, args.src_vector_dir, args.dst_dir)
