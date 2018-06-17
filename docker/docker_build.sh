27434796d9156884cb7b52712c9a1c93346827e5
motokimura <motokimura1@gmail.com>
2018-06-17 21:33:32 +0900

    Fix a comment

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -7,6 +7,6 @@ IMAGE="spacenet_building:test"
 THIS_DIR=$(cd $(dirname $0); pwd)
 PROJ_DIR=`dirname ${THIS_DIR}`
 
-# Buld docker container in project root directory
+# Buld docker image from project root directory
 cd ${PROJ_DIR} && \
 nvidia-docker build -t ${IMAGE} -f ${THIS_DIR}/Dockerfile .
